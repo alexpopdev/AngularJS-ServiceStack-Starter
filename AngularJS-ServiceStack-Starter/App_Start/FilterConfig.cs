ea88d558b9b1cc21ad573f0772e0e2feff8787cb
Alex Pop <palexandru@gmail.com>
2013-10-11 11:14:29 +0100

    initial add

---
+++ AngularJS-ServiceStack-Starter/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS_ServiceStack_Starter
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AngularJS-ServiceStack-Starter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJS_ServiceStack_Starter.MvcApplication" Language="C#" %>
